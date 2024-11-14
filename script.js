4eff510f48dce2d725206818e3bba372b2da62d0
Franklin188768 <franklinyarielmora21@gmail.com>
2024-11-14 12:12:11 -0500

    Working form

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello!');
+
+document.getElementById("loginForm").addEventListener("submit", formsubmission);
+
+function formsubmission(event) {
+    event.preventDefault();
+    console.log("Form submitted!");
+    window.location.href = "page2.html";
+}
+
+
